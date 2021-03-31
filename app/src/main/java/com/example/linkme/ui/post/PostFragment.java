b0d3fe6180544bf10f838b213d0ec6d22a50e25f
Julien Benac <julien.benac@iut-rodez.fr>
2021-03-31 14:54:36 +0200

    Première version

---
+++ app/src/main/java/com/example/linkme/ui/post/PostFragment.java
@@ -0,0 +1,23 @@
+package com.example.linkme.ui.post;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.linkme.R;
+
+public class PostFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_post, container, false);
+
+        return root;
+    }
+
+}
---
+++ app/src/main/java/com/example/linkme/ui/story/StoryFragment.java
@@ -0,0 +1,23 @@
+package com.example.linkme.ui.story;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.linkme.R;
+
+public class StoryFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_story, container, false);
+
+        return root;
+    }
+
+}
